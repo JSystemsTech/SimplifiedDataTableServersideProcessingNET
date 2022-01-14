75c8996af6ae94a2ab0157f6b6341316274ba1d4
Jonathan McGuire <JSystemsTech@gmail.com>
2022-01-14 02:04:53 -0500

    update code

--- PerformanceTesterApp/DataTableProcessor.cs
+++ PerformanceTesterApp/DataTableProcessor.cs
@@ -1,5 +1,4 @@
-﻿using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
@@ -14,11 +13,69 @@ namespace SimplifiedDataTableServersideProcessingNET
     {
         object MapValue(string propertyName);
     }
-    
+    public class Helper
+    {
+        private IDictionary<string, Func<object, object>> PropertyGetters { get; set; }
+
+        private IDictionary<string, Action<object, object>> PropertySetters { get; set; }
+
+        public static Func<object, object> CreateGetter(PropertyInfo property)
+        {
+            if (property == null)
+                throw new ArgumentNullException("property");
+
+            var getter = property.GetGetMethod();
+            if (getter == null)
+                throw new ArgumentException("The specified property does not have a public accessor.");
+
+            var genericMethod = typeof(Helper).GetMethod("CreateGetterGeneric");
+            MethodInfo genericHelper = genericMethod.MakeGenericMethod(property.DeclaringType, property.PropertyType);
+            return (Func<object, object>)genericHelper.Invoke(null, new object[] { getter });
+        }
+
+        public static Func<object, object> CreateGetterGeneric<T, R>(MethodInfo getter) where T : class
+        {
+            Func<T, R> getterTypedDelegate = (Func<T, R>)Delegate.CreateDelegate(typeof(Func<T, R>), getter);
+            Func<object, object> getterDelegate = (Func<object, object>)((object instance) => getterTypedDelegate((T)instance));
+            return getterDelegate;
+        }
+
+        public static Action<object, object> CreateSetter(PropertyInfo property)
+        {
+            if (property == null)
+                throw new ArgumentNullException("property");
+
+            var setter = property.GetSetMethod();
+            if (setter == null)
+                throw new ArgumentException("The specified property does not have a public setter.");
+
+            var genericMethod = typeof(Helper).GetMethod("CreateSetterGeneric");
+            MethodInfo genericHelper = genericMethod.MakeGenericMethod(property.DeclaringType, property.PropertyType);
+            return (Action<object, object>)genericHelper.Invoke(null, new object[] { setter });
+        }
+
+        public static Action<object, object> CreateSetterGeneric<T, V>(MethodInfo setter) where T : class
+        {
+            Action<T, V> setterTypedDelegate = (Action<T, V>)Delegate.CreateDelegate(typeof(Action<T, V>), setter);
+            Action<object, object> setterDelegate = (Action<object, object>)((object instance, object value) => { setterTypedDelegate((T)instance, (V)value); });
+            return setterDelegate;
+        }
+
+        public Helper(Type type)
+        {
+            var Properties = type.GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                        .Where(p => p.CanRead && !p.GetIndexParameters().Any()).AsEnumerable();
+            PropertyGetters = Properties.ToDictionary(p => p.Name, p => CreateGetter(p));
+            PropertySetters = Properties.Where(p => p.GetSetMethod() != null)
+                .ToDictionary(p => p.Name, p => CreateSetter(p));
+        }
+    }
     public class PropertyInfo<T>
         where T : class
     {
-        private static ConcurrentDictionary<string, Func<T, object>> Getters { get; set; }
+        private static Helper _Helper { get; set; }
+        private static Helper Helper = _Helper ?? new Helper(typeof(T));
+        private static ConcurrentDictionary<string, Func<object, object>> Getters { get; set; }
         private static IEnumerable<PropertyInfo> _Properties { get; set; }
         public static IEnumerable<PropertyInfo> Properties = _Properties ?? typeof(T).GetProperties(BindingFlags.Instance | BindingFlags.Public);
         public static object GetValue(T model, string name)
@@ -29,16 +86,14 @@ namespace SimplifiedDataTableServersideProcessingNET
             }
             if (Getters == null)
             {
-                Getters = new ConcurrentDictionary<string, Func<T, object>>();
+                Getters = new ConcurrentDictionary<string, Func<object, object>>();
             }
             
             if (!Getters.ContainsKey(name) && Properties.FirstOrDefault(p => p.Name == name) is PropertyInfo propInfo)
             {
-                MethodInfo method = propInfo.GetAccessors().First();
-                Func<T, object> dlg = (Func<T, object>) Delegate.CreateDelegate(typeof(Func<T, object>), method);
-                Getters.TryAdd(name, dlg);
+                Getters.TryAdd(name, Helper.CreateGetter(propInfo));
             }
-            if (Getters.TryGetValue(name, out Func<T, object> getter))
+            if (Getters.TryGetValue(name, out Func<object, object> getter))
             {
                 return getter(model);
             }
@@ -107,19 +162,19 @@ namespace SimplifiedDataTableServersideProcessingNET
             Func<T, object> getRowData = null,
             Func<T, object> getRowAttr = null) where T : class
         {
-            try
-            {
+            //try
+            //{
                 var filteredData = data.GeneralSearchDataTable(request).ColumnSpecificSearchDataTable<T>(request);
                 var orderedData = filteredData.OrderDataTable(request);
                 var pageData = orderedData.GetDataTablePage(request);
 
                 var serializedData = pageData.SerializeData(getRowId, getRowClass, getRowData, getRowAttr);
                 return new DataTableResponse(request, serializedData, data.Count(), filteredData.Count());
-            }
-            catch (Exception e)
-            {
-                return new DataTableResponse(request, e);
-            }
+            //}
+            //catch (Exception e)
+            //{
+                //return new DataTableResponse(request, e);
+            //}
         }
         public static IEnumerable<IDictionary> SerializeData<T>(
             this IEnumerable<T> data,
--- PerformanceTesterApp/Program.cs
+++ PerformanceTesterApp/Program.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace PerformanceTesterApp
 {
-    public class MyClass
+    public class MyClass//:IMappable
     {
         public Guid Guid { get; set; }
         public DateTime DateTime { get; set; }
@@ -22,19 +22,102 @@ namespace PerformanceTesterApp
             Guid = Guid.NewGuid();
             Guid2 = Guid.NewGuid();
             DateTime = DateTime.Now;
-            DateTime = DateTime.UtcNow;
+            DateTime2 = DateTime.UtcNow;
             Text = "Text";
-            Text = "Text2";
+            Text2 = "Text2";
         }
+
+        //public object MapValue(string propertyName)
+        //{
+        //    return propertyName == "Guid" ? Guid :
+        //    propertyName == "DateTime" ? DateTime :
+        //    propertyName == "Text" ? Text :
+        //    propertyName == "Number" ? Number :
+        //    propertyName == "Guid2" ? Guid2 :
+        //    propertyName == "DateTime2" ? DateTime2 :
+        //    propertyName == "Text2" ? Text2 :
+        //    propertyName == "Number2" ? Number2 : (object)null;
+        //}
+
     }
     public class Program
     {
         public static void Main()
         {
-            
+            DataTableRequest request = new DataTableRequest()
+            {
+                Draw = 1,
+                Start = 1,
+                Length = 100,
+                Order = new DataTableOrder[] { 
+                    new DataTableOrder() {
+                        Column = 0,
+                        Dir = "asc"
+                    },
+                    new DataTableOrder() {
+                        Column = 1,
+                        Dir = "desc"
+                    }
+                },
+                Columns = new DataTableColumn[] {
+                    new DataTableColumn() {
+                        Data = "Guid",
+                        Name = "Guid",
+                        Searchable = true,
+                        Orderable = true,
+                    },
+                    new DataTableColumn() {
+                        Data = "DateTime",
+                        Name = "DateTime",
+                        Searchable = true,
+                        Orderable = true,
+                    },
+                    new DataTableColumn() {
+                        Data = "Text",
+                        Name = "Text",
+                        Searchable = true,
+                        Orderable = true,
+                    },
+                    new DataTableColumn() {
+                        Data = "Number",
+                        Name = "Number",
+                        Searchable = true,
+                        Orderable = true,
+                    },
+                    new DataTableColumn() {
+                        Data = "Guid2",
+                        Name = "Guid2",
+                        Searchable = true,
+                        Orderable = true,
+                    },
+                    new DataTableColumn() {
+                        Data = "DateTime2",
+                        Name = "DateTime2",
+                        Searchable = true,
+                        Orderable = true,
+                    },
+                    new DataTableColumn() {
+                        Data = "Text2",
+                        Name = "Text2",
+                        Searchable = true,
+                        Orderable = true,
+                    },
+                    new DataTableColumn() {
+                        Data = "Number2",
+                        Name = "Number2",
+                        Searchable = true,
+                        Orderable = true,
+                    }
+                },
+                Search= new DataTableSearch()
+                {
+                    Value = "tex",
+                    Regex = false
+                }
+            };
             var data = Enumerable.Repeat(new MyClass(), 100000);
             DateTime a = DateTime.Now;
-            var test = data.SerializeData();
+            var test = data.ProcessDataTable(request);
             DateTime b = DateTime.Now;
             Console.WriteLine(b.Subtract(a).TotalMilliseconds);
             Console.ReadKey();
